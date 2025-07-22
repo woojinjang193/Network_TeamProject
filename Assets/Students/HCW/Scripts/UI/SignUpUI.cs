0f6c9ad4cb73be9e221c11d48cb934a0a1de76ed
CHEEWOO HONG <1974cldn@gmail.com>
2025-07-22 17:48:20 +0900

    [Feat] basic login, sign up UI layout
    로그인, 회원가입 기본 UI 구성
    TMP 추가, 한글 폰트 추가

---
+++ Assets/Students/HCW/Scripts/UI/SignUpUI.cs
@@ -0,0 +1,64 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUpUI : BaseUI
+{
+    [SerializeField] private TMP_InputField emailInputField;
+    [SerializeField] private TMP_InputField nameInputField;
+    [SerializeField] private TMP_InputField passwordInputField;
+    [SerializeField] private TMP_InputField confirmPasswordInputField;
+    [SerializeField] private Button registerButton;
+    [SerializeField] private Button backToLoginButton;
+    [SerializeField] private TMP_Text messageText;
+
+    private void Start()
+    {
+        registerButton.onClick.AddListener(OnRegisterButtonClicked);
+        backToLoginButton.onClick.AddListener(ShowLoginUI);
+    }
+
+    private void OnRegisterButtonClicked()
+    {
+        string email = emailInputField.text;
+        string name = nameInputField.text;
+        string password = passwordInputField.text;
+        string confirmPassword = confirmPasswordInputField.text;
+
+        if (password != confirmPassword)
+        {
+            SetMessage("비밀번호가 일치하지 않습니다.");
+            return;
+        }
+
+        SetMessage("회원가입 중..."); // 로딩 메시지 표시
+
+        // TODO: 여기에 Firebase 회원가입 로직을 추가할 예정
+        Debug.Log($"회원가입 시도: {email} / {name} / {password}");
+    }
+
+    private void ShowLoginUI()
+    {
+        FindObjectOfType<UIManager>().ShowUI(typeof(LoginUI));
+    }
+
+    // 호출할 메시지 업데이트 함수
+    public void SetMessage(string message)
+    {
+        if (messageText != null)
+        {
+            messageText.text = message;
+        }
+    }
+
+    public override void Open()
+    {
+        SetMessage(""); // 패널이 열릴 때 메시지 초기화
+        gameObject.SetActive(true);
+    }
+
+    public override void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Students/HCW/Scripts/UI/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private List<BaseUI> uiPanels;
+    [SerializeField] private BaseUI startPanel;
+    private BaseUI currentActivePanel;
+
+    // 시작시 모든 UI 패널 비활성화
+    private void Awake()
+    {
+        foreach (var panel in uiPanels)
+        {
+            panel.Close();
+        }
+    }
+
+    private void Start()
+    {
+        // 시작 패널을 활성화
+        if (startPanel != null)
+        {
+            ShowUI(startPanel.GetType());
+        }
+    }
+
+    // UI 활성화 메서드
+    public void ShowUI(System.Type uiType)
+    {
+        // 중복 방지
+        if (currentActivePanel != null && currentActivePanel.GetType() == uiType)
+        {
+            return;
+        }
+        // 열고자 하는 UI 타입과 일치하는 패널을 리스트에서 찾음
+        BaseUI uiToShow = uiPanels.FirstOrDefault(panel => panel.GetType() == uiType);
+
+        if (uiToShow != null)
+        {
+            if (currentActivePanel != null)
+            {
+                currentActivePanel.Close();
+            }
+            // 새 UI을 열고 현재 활성화 패널로 등록
+            uiToShow.Open();
+            currentActivePanel = uiToShow;
+        }
+    }
+
+    // 열려있는 UI 비활성화 메서드
+    public void CloseCurrentUI()
+    {
+        if (currentActivePanel != null)
+        {
+            currentActivePanel.Close();
+            currentActivePanel = null;
+        }
+    }
+}
