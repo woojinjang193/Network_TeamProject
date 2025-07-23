01eece692637f6efa68569894146e69f77440f9a
CHEEWOO HONG <1974cldn@gmail.com>
2025-07-23 11:17:54 +0900

    [Feat] Test UI
    로그인, 회원가입 기본 UI틀

---
+++ Assets/Students/HCW/Scripts/UI/InGameUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : BaseUI
+{
+    public override void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Students/HCW/Scripts/UI/LobbyUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyUI : BaseUI
+{
+    public override void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
