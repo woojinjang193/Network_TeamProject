a265e3f5b73f5e348b8d90d102c7bd4c55e042e7
antactics <142508000+antactics@users.noreply.github.com>
2025-07-30 12:01:04 +0900

    [chore] sync progress
    프로토타입 빌드를 위한 진행상태 통일

--- Assets/Students/LHG/Scripts/DetectModule.cs
+++ Assets/Students/LHG/Scripts/DetectModule.cs
@@ -8,7 +8,7 @@ public class DetectModule
     private AIController _controller;
     public Transform Target { get; private set; }
     public bool HasEnemy => Target != null;
-
+    private float detectRadius = 10f;
 
 
     public DetectModule(AIController controller)
@@ -18,15 +18,26 @@ public class DetectModule
 
     public void Update()
     {
+        DetectEnemy();
+        DetectGrid();
+    }
+
+    private void DetectEnemy()
+    {
         //오버랩스피어로 Player태그를 가진 collider중에서 첫번째 요소를 타겟으로 지정=FristOrDefaul
-        Collider[] hits = Physics.OverlapSphere(_controller.transform.position, 10f);
+        Collider[] hits = Physics.OverlapSphere(_controller.transform.position, detectRadius);
         Target = hits.FirstOrDefault(c => c.CompareTag("Player"))?.transform;
 
         if (Target != null)
         {
             Debug.Log("Player 태그 감지됨");
         }
-        //TODO 중립 또는 적 타일을 탐지 //5~10초, 타일판단을어떻게?
-        //타겟타일
+        
+        
+    }
+
+    private void DetectGrid()
+    {
+        //TODO 중립 또는 적 타일을 탐지 
     }
 }
