f54797d67b480647aefafe56d65c8e374a47f4ef
Dam1230 <dkssud4111@gmail.com>
2025-07-30 12:04:40 +0900

    feat: Add match timer and Firebase nickname upload
    -파이어베이스에 닉네임 업로드
    -게임 타이머 기능 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,14 +18,52 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private Transform[] team2SpawnPoints;   // Team2 스폰 
     [SerializeField] private string playerPrefabName = "Player";
 
+    private double startTime;
+    [SerializeField] private float matchDuration = 180f;
+    [SerializeField] private TMP_Text timerText;
+    private PhotonView photonView;
+
     protected override void Awake()
     {
         base.Awake();
+        photonView = GetComponent<PhotonView>();
         DontDestroyOnLoad(gameObject);
     }
     private void Start()
     {
         Debug.Log("게임매니저 스타트");
+    }
+
+    [PunRPC]
+    private void SetStartTime(double start)
+    {
+        startTime = start;
+        StartCoroutine(GameTimer());
+    }
+ 
+
+    private IEnumerator GameTimer()
+    {
+        while (true)
+        {
+            double elapsed = PhotonNetwork.Time - startTime;
+            float remaining = matchDuration - (float)elapsed;
+
+            if (remaining <= 0)
+                break;
+
+            UpdateTimerUI(remaining);
+            yield return null;
+        }
+
+        GameEnd();
+    }
+
+    private void UpdateTimerUI(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        timerText.text = $"{minutes:00}:{seconds:00}";
     }
 
     private IEnumerator SpawnPlayerWithDelay()
@@ -68,6 +107,11 @@ public class GameManager : Singleton<GameManager>
     public void GameStart()
     {
         Debug.Log("게임 스타트");
+        if (PhotonNetwork.IsMasterClient)
+        {
+            double start = PhotonNetwork.Time;
+            photonView.RPC("SetStartTime", RpcTarget.All, start);
+        }
     }
 
     public void GameEnd()
@@ -75,16 +119,16 @@ public class GameManager : Singleton<GameManager>
         Debug.Log("게임 엔드");
 
         // 현재 유저가 속한 팀 가져오기
-        //string myTeam = PhotonNetwork.LocalPlayer.CustomProperties["team"].ToString();
+        string myTeam = PhotonNetwork.LocalPlayer.CustomProperties["team"].ToString();
 
-        // 승리 팀 판단
-        //string winningTeam = GridManager.GetInstance().GetWinningTeam();
-        //Debug.Log($"내 팀: {myTeam}, 승리 팀: {winningTeam}");
+        //승리 팀 판단
+        string winningTeam = GridManager.Instance.GetWinningTeam();
+        Debug.Log($"내 팀: {myTeam}, 승리 팀: {winningTeam}");
 
         //팀이 이긴 경우
-       // bool isWin = (winningTeam != "Draw") && (myTeam == winningTeam);
+        bool isWin = (winningTeam != "Draw") && (myTeam == winningTeam);
 
-       // FirebaseManager.UploadMatchResult(isWin);
+        FirebaseManager.UploadMatchResult(isWin);
 
         SceneManager.LoadScene("LoginScene"); //씬 이름 변경 예정
     }
