fd3e23f595d22bdbfea9c511f920ca3bc2b73495
CHEEWOO HONG <1974cldn@gmail.com>
2025-08-01 17:21:01 +0900

    [Feat] Ink System UI
    UIManager :  싱글톤 상속받게 수정
    InGameUI :  HUD가 플레이어의 메인카메라를 찾고 따라다니게 구현
    잉크 시스템 : InkParticleGun에 잉크 잔량 설정 추가

--- Assets/Students/HCW/Scripts/UI/InGameUI.cs
+++ Assets/Students/HCW/Scripts/UI/InGameUI.cs
@@ -1,10 +1,15 @@
+using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class InGameUI : BaseUI
 {
+    private Canvas _canvas;
+    private PlayerController myPlayer; // 내 플레이어 참조
+
     [System.Serializable]
     public class PlayerStatusIcon
     {
@@ -32,15 +37,71 @@ public class InGameUI : BaseUI
     [SerializeField] private List<PlayerStatusIcon> teamA_Icons; // A팀
     [SerializeField] private List<PlayerStatusIcon> teamB_Icons; // B팀
 
+    void Awake()
+    {
+        _canvas = GetComponent<Canvas>();
+    }
+
+    private void Update()
+    {
+        // 내 플레이어를 아직 못 찾았다면 탐색
+        if (myPlayer == null)
+        {
+            FindMyPlayer();
+            if (myPlayer == null) return; // 아직도 못찾았으면 Update 종료
+        }
+
+        // 플레이어를 찾았다면, 잉크 게이지 업데이트
+        if (myPlayer.inkParticleGun != null)
+        {
+            UpdateInkGauge(myPlayer.inkParticleGun.currentInk, myPlayer.inkParticleGun.maxInk);
+        }
+    }
 
     public override void Open()
     {
         gameObject.SetActive(true);
+        StartCoroutine(FindAndSetPlayerCamera());
     }
 
     public override void Close()
     {
         gameObject.SetActive(false);
+    }
+
+    private void FindMyPlayer()
+    {
+        var players = FindObjectsOfType<PlayerController>();
+        foreach (var player in players)
+        {
+            if (player.photonView.IsMine)
+            {
+                myPlayer = player;
+                return; // 찾았으면 바로 종료
+            }
+        }
+    }
+
+    private IEnumerator FindAndSetPlayerCamera()
+    {
+        // 한 프레임 대기
+        yield return null;
+
+        if (myPlayer == null) FindMyPlayer();
+
+        if (myPlayer != null && myPlayer.mainCamera != null)
+        {
+            if (_canvas != null)
+            {
+                _canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                _canvas.worldCamera = myPlayer.mainCamera;
+                _canvas.planeDistance = 1; // 카메라와 UI 사이의 거리 (적절히 조절)
+            }
+        }
+        else
+        {
+            Debug.LogWarning("InGameUI: 내 플레이어 컨트롤러 또는 메인 카메라를 찾을 수 없습니다.");
+        }
     }
 
     // 남은 시간을 UI에 업데이트
@@ -54,7 +115,10 @@ public class InGameUI : BaseUI
     // 잉크 용량 업데이트
     public void UpdateInkGauge(float currentInk, float maxInk)
     {
-        slider_InkGauge.value = currentInk / maxInk;
+        if(slider_InkGauge != null)
+        {
+            slider_InkGauge.value = currentInk / maxInk;
+        }
     }
 
     // 잉크 게이지 활성화상태 설정 (상시 활성화 상태로 바꿀시 필요없어짐)
