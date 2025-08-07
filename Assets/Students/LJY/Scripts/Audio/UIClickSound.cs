e56551ee31cb7c59189c60425e903515054cc31b
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-08-07 12:53:41 +0900

    [Feat] prefab adjust
    버튼 UI에 클릭 소리 추가

---
+++ Assets/Students/LJY/Scripts/Audio/UIClickSound.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIClickSound : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        while (Manager.Audio == null)
+        {
+            yield return null;
+        }
+        Button btn = gameObject.GetComponent<Button>();
+        if (btn)
+        {
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(Manager.Audio.Click);
+        }
+    }
+
+}
